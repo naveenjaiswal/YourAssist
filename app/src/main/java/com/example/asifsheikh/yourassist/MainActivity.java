4a09661be013a6d54d0f159ffd4c2dca2c74bd1c
asifsheikh <sheikhaasif01@gmail.com>
2015-09-02 22:40:41 +0530

    Navigation Base Activity

--- app/src/main/java/com/example/asifsheikh/yourassist/MainActivity.java
+++ app/src/main/java/com/example/asifsheikh/yourassist/MainActivity.java
@@ -1,17 +1,19 @@
 package com.example.asifsheikh.yourassist;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.asifsheikh.yourassist.home.NavigationDrawer_Activity;
+
+public class MainActivity extends NavigationDrawer_Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
