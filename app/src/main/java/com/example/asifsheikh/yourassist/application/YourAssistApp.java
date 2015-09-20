657090e7d2abdce8c540d1b4b195169ff72a5f04
Asif Shaikh <sheikhaasif01@gmail.com>
2015-09-21 03:09:10 +0530

    Adding Application class

---
+++ app/src/main/java/com/example/asifsheikh/yourassist/application/YourAssistApp.java
@@ -0,0 +1,90 @@
+package com.example.asifsheikh.yourassist.application;
+
+import android.app.Application;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
+
+/**
+ * Created by Admin on 9/20/2015.
+ */
+public class YourAssistApp extends Application {
+
+    /* Singleton Object */
+    private static YourAssistApp mYourAssistApp;
+    private RequestQueue mRequestQueue;
+
+
+
+    /* Client for accessing Google APIs */
+    private  GoogleApiClient mGoogleApiClient;
+
+    private Person currentPerson;
+    private String Email;
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+   public Person getCurrentPerson() {
+        return currentPerson;
+    }
+
+    public void setCurrentPerson(Person currentPerson) {
+        this.currentPerson = currentPerson;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        mYourAssistApp = this;
+    }
+
+    public  synchronized static  YourAssistApp getAppInstance(){
+
+        return mYourAssistApp;
+
+    }
+
+    public YourAssistApp()
+    {
+        // Constructor hidden because this is a singleton
+        // [START create_google_api_client]
+        // Build GoogleApiClient with access to basic profile
+        /*mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Plus.API)
+                .addScope(new Scope(Scopes.PROFILE))
+                .build();*/
+        // [END create_google_api_client]
+    }
+
+    public GoogleApiClient getmGoogleApiClient(){
+        return mGoogleApiClient;
+    }
+
+    public  void  setmGoogleApiClient(GoogleApiClient mGoogleApiClient) {
+        mGoogleApiClient = mGoogleApiClient;
+    }
+
+    public RequestQueue getRequestQueue() {
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+}
